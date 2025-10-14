dc09cf4823514d9c4b7de59ce907446d85e447de
Sahil Sahu <sahilsahu731709@gmail.com>
2025-10-14 11:42:54 +0530

    setup database

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import http from 'http';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import http from "http";
+import connectDB from "./lib/db.js";
 
 dotenv.config();
 
@@ -9,22 +10,21 @@ const app = express();
 const server = http.createServer(app);
 
 // middleware
-app.use(express.json({ limit: '4mb' }));
+app.use(express.json({ limit: "4mb" }));
 app.use(cors());
 
 app.use("/api/status", (req, res) => {
   res.send("Server is running");
 });
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-
-
