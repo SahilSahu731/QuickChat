6052280102403ac797c80c00c47b5cd7246edb51
Sahil Sahu <sahilsahu731709@gmail.com>
2025-10-16 09:57:36 +0530

    complete everything

--- server/index.js
+++ server/index.js
@@ -15,27 +15,27 @@ const server = http.createServer(app);
 export const io = new Server(server, {
   cors: {
     origin: "*",
-  }
+  },
 });
 
 export const userSocketMap = {};
 
 // socket.io connection handler
 io.on("connection", (socket) => {
-    const userId = socket.handshake.query.userId;
-    console.log("user connected", userId)
-    if (userId) {
-        userSocketMap[userId] = socket.id;
-    }
-    // emit online users to all 
-    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  const userId = socket.handshake.query.userId;
+  console.log("user connected", userId);
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+  // emit online users to all
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", () => {
-        console.log("user disconnected", userId);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    })
-})
+  socket.on("disconnect", () => {
+    console.log("user disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
 
 // middleware
 app.use(express.json({ limit: "4mb" }));
@@ -44,7 +44,6 @@ app.use(cors());
 // api routes
 app.use("/api/auth", userRoutes);
 app.use("/api/messages", messageRoutes);
-
 
 app.use("/api/status", (req, res) => {
   res.send("Server is running");
@@ -58,6 +57,10 @@ connectDB();
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+export default server;
